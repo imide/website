3142cecc489e803c1a9019d84c57992e922a24c2
Patrick <37928912+smartfrigde@users.noreply.github.com>
2024-11-15 18:46:25 +0100

    1.0.4

--- public/getLatest.js
+++
@@ -1,6 +0,0 @@
-
-window.onload = async () => {
-    const response = await fetch("/latest.json");
-    const data = await response.json();
-    document.body.innerHTML = await document.body.innerHTML.replaceAll("VERSIONNUM", data.version);
-}
